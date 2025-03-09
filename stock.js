1a057f4c9fe28e58c870134870dfbb2f48b3a1cb
Daniel Briceno <daniel.b100726@gmail.com>
2025-03-09 00:13:44 -0400

    modularizando javascript

--- stock.js
+++
@@ -1,8 +0,0 @@
-let stockProductos = [
-    {id: 1, nombre: "Corona pack x6", tipo: "buzo", cantidad: 1, desc: "Cerveza premium, clásica y auténtica.", contenido: "330ml x6", precio: 1669, img: './img/corona-sixpack.jpg'},
-    {id: 2, nombre: "Fernet Branca", tipo: "buzo", cantidad: 1, desc: "Se elabora a partir de la combinación de 27 plantas y especias.", contenido: "750ml", precio: 1349, img: './img/fernet-branca.jpg'},
-    {id: 3, nombre: "Mar del Plata Gin", tipo: "buzo", cantidad: 1, desc: "Elaborado y destilado artesanalmente.", contenido: "750ml", precio: 7400, img: './img/mar-del-plata-gin.png'},
-    {id: 4, nombre: "Andes Origen Roja pack x6", tipo: "buzo", cantidad: 1, desc: "Cerveza roja vienna lager", contenido: "330ml x6", precio: 1059, img: './img/cerveza-andes.png'},
-    {id: 5, nombre: "bebida 5", tipo: "buzo", cantidad: 1, desc: "Cerveza roja vienna lager", contenido: "330ml x6", precio: 1200, img: './img/cerveza-andes.png'},
-    {id: 6, nombre: "bebida 6", tipo: "buzo", cantidad: 1, desc: "Cerveza roja vienna lager", contenido: "330ml x6", precio: 1500, img: './img/cerveza-andes.png'},
-]
