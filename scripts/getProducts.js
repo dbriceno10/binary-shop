b2543fba77c8896d8332a6900ab324a20f144d47
Daniel Briceno <daniel.b100726@gmail.com>
2025-03-09 00:17:42 -0400

    modularzando json

--- scripts/getProducts.js
+++ scripts/getProducts.js
@@ -1,5 +1,5 @@
 const getProducts = async () => {
-  const data = await fetch('./productos.json');
+  const data = await fetch('../json/productos.json');
   const products = await data.json();
   return products;
 };
